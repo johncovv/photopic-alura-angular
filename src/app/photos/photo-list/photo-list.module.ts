492cf28c826991151ca66247c36a8dff15c5682a
John Covv <contato@johncovv.com>
2021-02-01 18:13:18 -0300

    creating directive for change appearance of DOM element

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 import { PhotoModule } from '../photo/photo.module';
 
 import { FilterByDescription } from './filter-by-description.pipe';
@@ -10,7 +11,7 @@ import { PhotosComponent } from './photos/photos.component';
 import { SearchComponent } from './search/search.component';
 
 @NgModule({
-	imports: [CommonModule, PhotoModule, CardModule],
+	imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule],
 	declarations: [
 		PhotoListComponent,
 		PhotosComponent,
---
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -0,0 +1,32 @@
+import {
+	Directive,
+	ElementRef,
+	HostListener,
+	Input,
+	Renderer2,
+} from '@angular/core';
+
+@Directive({
+	selector: '[appDarkenOnHover]',
+})
+export class DarkenOnHoverDirective {
+	@Input() brightness: string = (70).toString();
+
+	constructor(private el: ElementRef, private render: Renderer2) {}
+
+	// decorator: on mouse over, execulte darkenOn
+	@HostListener('mouseover')
+	darkenOn() {
+		this.render.setStyle(
+			this.el.nativeElement,
+			'filter',
+			`brightness(${this.brightness}%)`
+		);
+	}
+
+	// decorator: on mouse leave, execulte darkenOff
+	@HostListener('mouseleave')
+	darkenOff() {
+		this.render.setStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
+	}
+}
